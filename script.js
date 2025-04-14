2c3fa9328799922e28ab849a6238b6b988cbc31e
KanielOutis <87232073+rubiess000027@users.noreply.github.com>
2025-04-14 16:32:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const text = "Seni seviyorum Azra";
+const text = "Seni seviyorum Mavissimmmm";
 let i = 0;
 const speed = 100;
 
@@ -7,7 +7,15 @@ function typeWriter() {
         document.getElementById("main-text").innerHTML += text.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
+    } else {
+        // Yazı tamamlandıktan sonra kalbi göster
+        showHeart();
     }
+}
+
+function showHeart() {
+    const heart = document.getElementById("heart");
+    heart.classList.add("show-heart");
 }
 
 window.onload = typeWriter;
